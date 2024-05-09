8436bd8497ed83971cd2c846bf4c91c317b1abf2
thatswhatsimonsaid <59618491+thatswhatsimonsaid@users.noreply.github.com>
2024-05-09 16:10:13 -0700

    Update FindThetaPop.R

--- Code/scripts/FindThetaPop.R
+++ Code/scripts/FindThetaPop.R
@@ -12,7 +12,7 @@ library(distr)
 rm(list=ls())
 TypeSetting = "Linear"
 set.seed(420)
-source("Code/functions/SimData",TypeSetting,".R")
+source("Code/functions/SimData",TypeSetting,".R", local = FALSE)
 
 NSim = 100000
 ParameterVector = cbind(MisspecVec = c(rep(0,16),rep(1,16)),                         # Delta: Misspecification Rate
