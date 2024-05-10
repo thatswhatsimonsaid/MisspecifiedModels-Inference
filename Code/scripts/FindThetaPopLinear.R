58ecfc9f7bcac5bc668754e153f6c5e46ab9d3d2
thatswhatsimonsaid <59618491+thatswhatsimonsaid@users.noreply.github.com>
2024-05-09 18:22:00 -0700

    Update FindThetaPopLinear.R

--- Code/scripts/FindThetaPopLinear.R
+++ Code/scripts/FindThetaPopLinear.R
@@ -12,9 +12,39 @@ library(distr)
 rm(list=ls())
 TypeSetting = "Linear"
 set.seed(420)
-# source("Stats572/Code/functions/SimData",TypeSetting,".R")
 
-NSim = 1000000
+### Data Generating Process ###
+SimDataLinear = function(N, rho, K, delta, gamma){
+  ### Summary: Simulates Data according to the parameters of 
+      # Abadie, Imbens, and Zhang (2014)
+  ### Inputs:
+    # N: Number of observations
+    # rho: Mixture/leverage parameter
+    # K: Number of covariates. Note the first covariate will always be 
+        # a mixture of a standard normal and log normal(0,0.5^2)
+    # delta: Misspecification parameter
+    # gamma: Heterogeneity parameter
+  ### Output:
+    # dat: A data set
+  
+  ### Covariates ###
+  X1 = sample(c(rnorm(N*(1-rho), mean = 0, sd = 1),
+                rlnorm(N*rho, meanlog = 0, sdlog = sqrt(0.5))))
+  X2K = sapply(1:(K-1), function(x) rnorm(n = N, mean = 0, sd = 1))
+  
+  ### Response ###
+  mu_i = X1 + delta * (X1^2 - 1)
+  sigma_i = exp(1-gamma*X1)
+  Y = rnorm(n = N, mean = mu_i, sd = sigma_i)
+
+  ### Return ###
+  if(K == 1){dat = data.frame(Y, X1)}else if(K>1){dat = data.frame(Y, X1, X2K)}
+  colnames(dat) = c("Y",paste0("X", 1:K))
+  return(list(dat = dat,
+              mu = mu_i))
+}
+               
+NSim = 100
 ParameterVector = cbind(MisspecVec = c(rep(0,16),rep(1,16)),                         # Delta: Misspecification Rate
                         HomoskedVec = rep(c(rep(0,8), rep(0.5,8)),2),                # Gamma: Heteroskedasticity Rate
                         SizeVec = rep(c(rep(50,4), rep(200,4)),4),                   # N: Observations
