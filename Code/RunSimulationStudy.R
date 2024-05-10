c036caa8c7a268b2a688c82384629de300f1b717
Simon D N <simondn@uw.edu>
2024-05-10 13:00:17 -0700

    Update cluster

--- Code/RunSimulationStudy.R
+++ Code/RunSimulationStudy.R
@@ -1,4 +1,6 @@
-### Libraries ###
+# Set Up
+## Libr.
+{r message=FALSE, warning=FALSE}
 library(stats)
 library(tidyverse)
 library(StatMatch)
@@ -8,47 +10,59 @@ library(sandwich)
 library(mixtools)
 library(distr)
 
+
 ## Rm. Var.
 rm(list=ls())
+dir = "/Users/simondn/Documents/Stats572/"
+
 
 # Functions
-source("Code/AIZResults.R")
-source("Code/ConfidenceIntervalFunction.R")
-source("Code/OneIterationFunction.R")
-source("Code/SimDataLinear.R")
-source("Code/SimDataLogistic.R")
-source("Code/SimulationFunction.R")
-source("Code/SimulationReformatResultsFunction.R")
-source("Code/ThetaCondFunction.R")
-source("Code/ThetaPopFunction.R")
-source("Code/VHatCondFunction.R")
-source("Code/VHatPopFunction.R")
-source("Code/WhichMinFunction.R")
+source("Code/functions/AIZResults.R")
+source("Code/functions/ConfidenceIntervalFunction.R")
+source("Code/functions/OneIterationFunction.R")
+source("Code/functions/SimDataLinear.R")
+source("Code/functions/SimDataLogistic.R")
+source("Code/functions/SimulationFunction.R")
+source("Code/functions/SimulationReformatResultsFunction.R")
+source("Code/functions/ThetaCondFunction.R")
+source("Code/functions/ThetaPopFunction.R")
+source("Code/functions/VHatCondFunction.R")
+source("Code/functions/VHatPopFunction.R")
+source("Code/functions/WhichMinFunction.R")
 
-# Simulation
+
+# Set Up
+
 ### Set Up ###
 set.seed(420)
-ParameterVector = cbind(MisspecVec = c(rep(0,16),rep(1,16)),                         # Delta: Misspecification Rate
+TypeSetting = "Linear"
+ParameterVector = cbind(MisspecVec = c(rep(0,16),rep(1,16)),                        # Delta: Misspecification Rate
                         HomoskedVec = rep(c(rep(0,8), rep(0.5,8)),2),                # Gamma: Heteroskedasticity Rate
                         SizeVec = rep(c(rep(50,4), rep(200,4)),4),                   # N: Observations
                         LeverageVec = rep(c(0,0,0.1,0.1),8),                         # rho: Mixture Leverage
                         KVec = rep(c(1,5),16)                                        # K: Parameters
-) %>% data.frame
+                        ) %>% data.frame
+
+
+# Simulation
 
 ### Run Simulation ###
-NSim = 100
+NSim = 10
 SimulationResultsList = list(length = nrow(ParameterVector))
 for(i in 1:nrow(ParameterVector)){
   print(paste0("Case ",i))
-  SimulationResultsList[[i]] = SimulationFunction(NSim = NSim, 
-                                                  ParameterVector = ParameterVector, 
-                                                  SimulationCase = i, 
-                                                  VarFixed = NA,
-                                                  TypeSetting = TypeSetting)  #write.csv(SimulationResultsList[[i]]$SimulationSEResults, file ="data/SimulationCases/Linear/StandardErrors/Case",i,".csv")
-  # write.csv(SimulationResultsList[[i]]$SimCoverageFrequency, file = "data/SimulationCases/Linear/Coverage/Case",i,".csv")
+SimulationResultsList[[i]] = SimulationFunction(NSim = NSim, 
+                                                ParameterVector = ParameterVector, 
+                                                SimulationCase = i, 
+                                                VarFixed = NA,
+                                                TypeSetting = TypeSetting)
+  # write.csv(SimulationResultsList[[i]]$SimulationSEResults, 
+            # file =  "data/SimulationCases/",TypeSetting,"/StandardErrors/Case",i,".csv"))
+  # write.csv(SimulationResultsList[[i]]$SimCoverageFrequency, 
+            # file =  "data/SimulationCases/",TypeSetting,"/Coverage/Case",i,".csv"))
 }
 names(SimulationResultsList) = paste0("Case",1:nrow(ParameterVector))
-saveRDS(SimulationResultsList, file = "data/SimulationCases/Linear/SimulationResults.rds")
+# saveRDS(SimulationResultsList, file = "data/SimulationCases/",TypeSetting,"/SimulationResults.rds"))
 
 
 
