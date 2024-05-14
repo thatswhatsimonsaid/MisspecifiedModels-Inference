63c2817b43753e9c89c9734c9759e6ddd9391318
Simon D N <simondn@uw.edu>
2024-05-14 16:15:20 -0700

    Add FindThetaPop simulations

--- Code/ThetaPopFunction.R
+++ Code/ThetaPopFunction.R
@@ -12,8 +12,48 @@ ThetaPopFunction = function(dat, rho, K, delta, gamma, TypeSetting){
 
   
   ### ThetaPop ###
-  if(delta==0 & gamma==0 & 
-     )
+  
+  for(i in 1:nrow(ParameterVector)){
+    delta = ParameterVector$MisspecVec[i]
+    gamma = ParameterVector$HomoskedVec[i]
+    N = ParameterVector$SizeVec[i]
+    rho = ParameterVector$LeverageVec[i]
+    K = ParameterVector$KVec[i]
+  
+  if(delta==0 & gamma==0 & N==50 & rho==0 & K==1){ThetaPop =1} #1
+  if(delta==0 & gamma==0 & N==50 & rho==0 & K==5){ThetaPop =1} #2
+  if(delta==0 & gamma==0 & N==50 & rho==0.1 & K==1){ThetaPop =1} #3
+  if(delta==0 & gamma==0 & N==50 & rho==0.1 & K==5){ThetaPop =1} #4
+  if(delta==0 & gamma==0 & N==200 & rho==0 & K==1){ThetaPop =1} #5
+  if(delta==0 & gamma==0 & N==200 & rho==0 & K==5){ThetaPop =1} #6
+  if(delta==0 & gamma==0 & N==200 & rho==0.1 & K==1){ThetaPop =1} #7
+  if(delta==0 & gamma==0 & N==200 & rho==0.1 & K==5){ThetaPop =1} #8
+  if(delta==0 & gamma==0.5 & N==50 & rho==0 & K==1){ThetaPop =1} #9
+  if(delta==0 & gamma==0.5 & N==50 & rho==0 & K==5){ThetaPop =1} #10
+  if(delta==0 & gamma==0.5 & N==50 & rho==0.1 & K==1){ThetaPop =1} #11
+  if(delta==0 & gamma==0.5 & N==50 & rho==0.1 & K==5){ThetaPop =1} #12
+  if(delta==0 & gamma==0.5 & N==200 & rho==0 & K==1){ThetaPop =1} #13
+  if(delta==0 & gamma==0.5 & N==200 & rho==0 & K==5){ThetaPop =1} #14
+  if(delta==0 & gamma==0.5 & N==200 & rho==0.1 & K==1){ThetaPop =1} #15
+  if(delta==0 & gamma==0.5 & N==200 & rho==0.1 & K==5){ThetaPop =1} #16
+  if(delta==1 & gamma==0 & N==50 & rho==0 & K==1){ThetaPop =1} #17
+  if(delta==1 & gamma==0 & N==50 & rho==0 & K==5){ThetaPop =1} #18
+  if(delta==1 & gamma==0 & N==50 & rho==0.1 & K==1){ThetaPop =1.6} #19
+  if(delta==1 & gamma==0 & N==50 & rho==0.1 & K==5){ThetaPop =1.6} #20
+  if(delta==1 & gamma==0 & N==200 & rho==0 & K==1){ThetaPop =1} #21
+  if(delta==1 & gamma==0 & N==200 & rho==0 & K==5){ThetaPop =1} #22
+  if(delta==1 & gamma==0 & N==200 & rho==0.1 & K==1){ThetaPop =1.7} #23
+  if(delta==1 & gamma==0 & N==200 & rho==0.1 & K==5){ThetaPop =1.7} #24
+  if(delta==1 & gamma==0.5 & N==50 & rho==0 & K==1){ThetaPop =1} #25
+  if(delta==1 & gamma==0.5 & N==50 & rho==0 & K==5){ThetaPop =1} #26
+  if(delta==1 & gamma==0.5 & N==50 & rho==0.1 & K==1){ThetaPop =1.6} #27
+  if(delta==1 & gamma==0.5 & N==50 & rho==0.1 & K==5){ThetaPop =1.6} #28
+  if(delta==1 & gamma==0.5 & N==200 & rho==0 & K==1){ThetaPop =1} #29
+  if(delta==1 & gamma==0.5 & N==200 & rho==0 & K==5){ThetaPop =1} #30
+  if(delta==1 & gamma==0.5 & N==200 & rho==0.1 & K==1){ThetaPop =1.7} #31
+  if(delta==1 & gamma==0.5 & N==200 & rho==0.1 & K==5){ThetaPop =1.7} #32
 
+   print(paste0("Case ", i, ": ",ThetaPop))
+  }
   return(ThetaPop)
 }
