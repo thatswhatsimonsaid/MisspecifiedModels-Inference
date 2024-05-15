5eee96f5fe17d17f9224708c642249010bef851b
Simon D N <simondn@uw.edu>
2024-05-14 19:09:38 -0700

    Lower NSim again

--- Code/RunSimulationStudyNew.R
+++ Code/RunSimulationStudyNew.R
@@ -58,7 +58,7 @@ ParameterVector= data.frame(MisspecVec = MisspecVec,
 ### Simulation ###
 set.seed(1)
 # NSim = 50000
-NSim = 500
+NSim = 100
 SimulationResults = SimulationFunction(NSim = NSim, 
                                        ParameterVector = ParameterVector, 
                                        SimulationCase = 1, 
