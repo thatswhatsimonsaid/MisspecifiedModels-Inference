36a65cbf7d4260afd2f8656e5b06b217eb52693d
Simon D N <simondn@uw.edu>
2024-05-23 11:30:33 -0700

    ThetaPop Updated and SLURM OOM

--- Code/ThetaPopFunction.R
+++ Code/ThetaPopFunction.R
@@ -1,29 +1,39 @@
-ThetaPopFunction = function(dat, rho, N, K, delta, gamma, TypeSetting){
+ThetaPopFunction = function(delta, rho, N, gamma, K, TypeSetting){
   
   ### Summary: Calculates the population regression estimate 
-  # according to Abadie, Imbens, Zheng (2014)
+      # according to Abadie, Imbens, Zheng (2014)
   ### Input: 
-  # dat: Data set. Particularly, we only need covariates X
   # delta: Misspecification parameter
   # rho: Leverage parameter
   ### Output:
   # Population regression estimate
-  
-  ### Simulation Cases ###
-  SimulationCase = SimulationCaseFunction(delta, gamma, N, rho, K)
-  
+
   if(TypeSetting == "Linear"){
-    # ThetaPopList = readRDS("/Users/simondn/Documents/Stats572/data/Linear/EstimatedThetaPop/EstimatedThetaPop.rds")
+    ThetaPopList = readRDS("/Users/simondn/Documents/Stats572/data/Linear/EstimatedThetaPop/EstimatedThetaPop.rds")
     # ThetaPopList = readRDS("data/Linear/EstimatedThetaPop/EstimatedThetaPop.rds")
+    ThetaPop = filter(ThetaPopList, delta == delta, rho == rho, N == N, gamma == gamma, K == K)$ThetaPop
+  }
     
-    if(SimulationCase <= 18){ThetaPop = 1.0}
-    if(SimulationCase %in% c(21,22,25,26,29,30)){ThetaPop = 1.0}
-    if(SimulationCase %in% c(19,20,27,28)){ThetaPop = 1.5}
-    if(SimulationCase %in% c(23,24,31,32)){ThetaPop = 1.6}
-  }else if(TypeSetting == "Logistic"){
-    ThetaPopList = readRDS("/Users/simondn/Documents/Stats572/data/Logistic/EstimatedThetaPop/EstimatedThetaPop.rds")
+  else if(TypeSetting == "Logistic"){
+    # ThetaPopList = readRDS("/Users/simondn/Documents/Stats572/data/Logistic/EstimatedThetaPop/EstimatedThetaPop.rds")
     # ThetaPopList = readRDS("data/Logistic/EstimatedThetaPop/EstimatedThetaPop.rds")
   }
-  
+ 
   return(ThetaPop)
 }
+
+
+# ThetaPopList = readRDS("/Users/simondn/Documents/Stats572/data/Linear/EstimatedThetaPop/EstimatedThetaPop.rds")
+# for(i in 1:nrow(ParameterVector)){
+#   print(paste0("Case: ",i))
+#   
+#   delta = ParameterVector$MisspecVec[i]
+#   rho = ParameterVector$LeverageVec[i]
+#   N = ParameterVector$SizeVec[i]
+#   gamma = ParameterVector$HomoskedVec[i]
+#   K = ParameterVector$KVec[i]
+#   ThetaPopOutput = ThetaPopFunction(rho = rho, N = N, K = K, delta = delta, gamma = gamma, "Linear")
+#   ThetaPopExpected = ThetaPopList[i,]$ThetaPop
+#   print(c(i,ThetaPopOutput,ThetaPopExpected))
+#   print('---')
+# }
