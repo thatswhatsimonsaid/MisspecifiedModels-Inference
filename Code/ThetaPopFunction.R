1e885d2ae65e721fb898295e912e4bfa86127932
Simon D N <simondn@uw.edu>
2024-05-24 19:19:57 -0700

    Working on Logistic Sims

--- Code/ThetaPopFunction.R
+++ Code/ThetaPopFunction.R
@@ -9,22 +9,22 @@ ThetaPopFunction = function(delta_i, rho_i, N_i, gamma_i, K_i, TypeSetting){
   # Population regression estimate
 
   if(TypeSetting == "Linear"){
-    # ThetaPopList = readRDS("/Users/simondn/Documents/Stats572/data/Linear/EstimatedThetaPop/EstimatedThetaPop.rds")
-    ThetaPopList = readRDS("data/Linear/EstimatedThetaPop/EstimatedThetaPop.rds")
-    ThetaPop = ThetaPopList %>% 
-      filter(delta == delta_i) %>%
-      filter(rho == rho_i) %>%
-      filter(N == N_i) %>%
-      filter(gamma == gamma_i) %>%
-      filter(K == K_i) %>%
-      select(ThetaPop) %>%
-      as.numeric()
-  }
-    
-  else if(TypeSetting == "Logistic"){
-    # ThetaPopList = readRDS("/Users/simondn/Documents/Stats572/data/Logistic/EstimatedThetaPop/EstimatedThetaPop.rds")
+    ThetaPopList = readRDS("/Users/simondn/Documents/Stats572/data/Linear/EstimatedThetaPop/EstimatedThetaPop.rds")
+    # ThetaPopList = readRDS("data/Linear/EstimatedThetaPop/EstimatedThetaPop.rds")
+  }else if(TypeSetting == "Logistic"){
+    ThetaPopList = readRDS("/Users/simondn/Documents/Stats572/data/Logistic/EstimatedThetaPop/EstimatedThetaPop.rds")
     # ThetaPopList = readRDS("data/Logistic/EstimatedThetaPop/EstimatedThetaPop.rds")
   }
+  
+  ### Assign Theta Pop ###
+  ThetaPop = ThetaPopList %>% 
+    filter(delta == delta_i) %>%
+    filter(rho == rho_i) %>%
+    filter(N == N_i) %>%
+    filter(gamma == gamma_i) %>%
+    filter(K == K_i) %>%
+    select(ThetaPop) %>%
+    as.numeric()
  
   return(ThetaPop)
 }
