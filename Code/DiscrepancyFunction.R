783230a0e50c7b6902cb4691f0b809fc4033659e
Simon D N <simondn@uw.edu>
2024-05-25 16:31:23 -0700

    Rerun N500_2000 linear

--- Code/DiscrepancyFunction.R
+++ Code/DiscrepancyFunction.R
@@ -1,14 +1,14 @@
 DiscrepancyFunction = function(epsilon, AIZResults, SimSEMedianList, CoverageList){
-
+  
   ### Discrepancy ###
   Discrepancy = list()
-  Discrepancy$SEPop = as.matrix(abs(AIZ$MedianSEPop - SimSEMedianList$Population))
-  Discrepancy$SECond = as.matrix(abs(AIZ$MedianSECond - SimSEMedianList$Conditional))
-  Discrepancy$CoveragePopPop = as.matrix(abs(AIZ$ThetaPopVarPop - CoverageList$ThetaPop_VPop))
-  Discrepancy$CoveragePopCond = as.matrix(abs(AIZ$ThetaPopVarCond - CoverageList$ThetaPop_VCond))
-  Discrepancy$CoverageCondPop = as.matrix(abs(AIZ$ThetaCondVarPop - CoverageList$ThetaCond_VPop))
-  Discrepancy$CoverageCondCond = as.matrix(abs(AIZ$ThetaCondVarCond - CoverageList$ThetaCond_VCond))
-  Discrepancy$Difference = as.matrix(abs(AIZ$Difference - SimSEMedianList$Difference))
+  Discrepancy$SEPop = as.matrix(abs(AIZResults$MedianSEPop - SimSEMedianList$Population))
+  Discrepancy$SECond = as.matrix(abs(AIZResults$MedianSECond - SimSEMedianList$Conditional))
+  Discrepancy$CoveragePopPop = as.matrix(abs(AIZResults$ThetaPopVarPop - CoverageList$ThetaPop_VPop))
+  Discrepancy$CoveragePopCond = as.matrix(abs(AIZResults$ThetaPopVarCond - CoverageList$ThetaPop_VCond))
+  Discrepancy$CoverageCondPop = as.matrix(abs(AIZResults$ThetaCondVarPop - CoverageList$ThetaCond_VPop))
+  Discrepancy$CoverageCondCond = as.matrix(abs(AIZResults$ThetaCondVarCond - CoverageList$ThetaCond_VCond))
+  Discrepancy$Difference = as.matrix(abs(AIZResults$Difference - SimSEMedianList$Difference))
   
   ### Indices ###
   LargeDiscrepancyIndices = list()
