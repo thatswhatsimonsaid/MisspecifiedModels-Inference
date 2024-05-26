c3424d9a7b6c946bab8863993876af98af5086a9
Simon D N <simondn@uw.edu>
2024-05-25 21:01:34 -0700

    Fixed typo

--- Code/CreateParameterVectorFTP.R
+++ Code/CreateParameterVectorFTP.R
@@ -30,7 +30,7 @@ write.csv(FTP_Combinations, file = "/Users/simondn/Documents/Stats572/data/Linea
 ### Run SLURM Simulations ###
 Simulation_Combinations = ExpandGridCombinations %>%
   mutate(JobName = paste0("Sim_",TypeSetting,"_D",MisspecVec,"_G",HomoskedVec,"_N",SizeVec,"_R",LeverageVec,"_K",KVec),
-         Output = paste0("Results/",TypeSetting,"/Linear/LargeSampleExtension/sim_D",MisspecVec,"_G",HomoskedVec,"_N",SizeVec,"_R",LeverageVec,"_K",KVec, ".rds"))
+         Output = paste0("Results/",TypeSetting,"/LargeSampleExtension/sim_D",MisspecVec,"_G",HomoskedVec,"_N",SizeVec,"_R",LeverageVec,"_K",KVec, ".rds"))
 write.csv(Simulation_Combinations, file = "/Users/simondn/Documents/Stats572/data/Linear/Parameters/ParameterVectorALL.csv")
 
 
