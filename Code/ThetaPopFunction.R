65bcddbe65afe5ced3c1fa383972a5c8a55ef0bc
Simon D N <simondn@uw.edu>
2024-05-27 23:39:50 -0700

    Decrease Nsim

--- Code/ThetaPopFunction.R
+++ Code/ThetaPopFunction.R
@@ -9,11 +9,11 @@ ThetaPopFunction = function(delta_i, rho_i, N_i, gamma_i, K_i, TypeSetting){
   # Population regression estimate
 
   if(TypeSetting == "Linear"){
-    ThetaPopList = readRDS("/Users/simondn/Documents/Stats572/data/Linear/EstimatedThetaPop/EstimatedThetaPop.rds")
-    # ThetaPopList = readRDS("data/Linear/EstimatedThetaPop/EstimatedThetaPop.rds")
+    # ThetaPopList = readRDS("/Users/simondn/Documents/Stats572/data/Linear/EstimatedThetaPop/EstimatedThetaPop.rds")
+    ThetaPopList = readRDS("data/Linear/EstimatedThetaPop/EstimatedThetaPop.rds")
   }else if(TypeSetting == "Logistic"){
-    ThetaPopList = readRDS("/Users/simondn/Documents/Stats572/data/Logistic/EstimatedThetaPop/EstimatedThetaPop.rds")
-    # ThetaPopList = readRDS("data/Logistic/EstimatedThetaPop/EstimatedThetaPop.rds")
+    # ThetaPopList = readRDS("/Users/simondn/Documents/Stats572/data/Logistic/EstimatedThetaPop/EstimatedThetaPop.rds")
+    ThetaPopList = readRDS("data/Logistic/EstimatedThetaPop/EstimatedThetaPop.rds")
   }
   
   ### Assign Theta Pop ###
