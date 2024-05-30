d71c6a0a65ba8519598f3e975c2e49a837497ad4
Simon D N <simondn@uw.edu>
2024-05-29 18:25:29 -0700

    FTP for super large N

--- Code/CreateParameterVectorFTP.R
+++ Code/CreateParameterVectorFTP.R
@@ -4,7 +4,7 @@ rm(list=ls())
 TypeSetting = "Linear"
 ExpandGridCombinations= expand.grid(MisspecVec = c(0,1),
                                     HomoskedVec = c(0,0.5),
-                                    SizeVec = seq(50, 20000, by = 150),
+                                    SizeVec = seq(50, 10000, by = 150),
                                     LeverageVec = c(0,0.1),
                                     KVec = c(1,5),
                                     TypeSetting = "Linear") %>% data.frame()
@@ -21,8 +21,3 @@ Simulation_Combinations = ExpandGridCombinations %>%
   mutate(JobName = paste0("Sim_",TypeSetting,"_D",MisspecVec,"_G",HomoskedVec,"_N",SizeVec,"_R",LeverageVec,"_K",KVec),
          Output = paste0("Results/",TypeSetting,"/LargeSampleExtension/sim_D",MisspecVec,"_G",HomoskedVec,"_N",SizeVec,"_R",LeverageVec,"_K",KVec, ".rds"))
 write.csv(Simulation_Combinations, file = "/Users/simondn/Documents/Stats572/data/Linear/Parameters/ParameterVectorALL.csv")
-
-
-
-
-
